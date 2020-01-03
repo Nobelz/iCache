33aaeea93bd916a65cff16bfed1f560989c8849e
Nobel Zhou <nobelzhou19@gmail.com>
2020-01-03 12:04:51 -0500

    Version 0.0.3: Fix inconsistency AGAIN.

--- iCache/Controllers/LoginViewController.swift
+++ iCache/Controllers/LoginViewController.swift
@@ -2,22 +2,12 @@
 //  LoginViewController.swift
 //  iCache
 //
-//  Created by Nobel Zhou on 1/2/20.
+//  Created by Nobel Zhou on 1/3/20.
 //  Copyright © 2020 Nobel Zhou. All rights reserved.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var emailTextField: UITextField!
     
-    @IBOutlet var passwordTextField: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-    @IBAction func loginPressed(_ sender: UIButton) {
-    }
 }
